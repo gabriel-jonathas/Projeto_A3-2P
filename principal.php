8b16440b067e75d9b6446df113c8666769c6039b
GABRIEL OLIVEIRA <127751781+gabriel-jonathas@users.noreply.github.com>
2023-11-07 10:30:27 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -4,6 +4,22 @@ session_start();
 
 include('conexao.php');
 
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <a href="addusuario.php">
+        Adicionar Usuário</a>
+    </center>
+    
+</body>
+</html>
