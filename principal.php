fad61d8f405061343c6e6d9c009004b7f4877578
GABRIEL OLIVEIRA <127751781+gabriel-jonathas@users.noreply.github.com>
2023-11-14 09:57:00 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -3,6 +3,10 @@
 session_start();
 
 include('conexao.php');
+include('validarlogin.php');
+
+$nivel = $_SESSION['nivel'];
+
 
 ?>
 
@@ -12,13 +16,20 @@ include('conexao.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
 <body>
     <center>
-        <a href="addusuario.php">
+        <?php
+        if($nivel < 3) { ?>
+        <a href="addusuarios.php">
         Adicionar Usuário</a>
+        <?php }
+      
+        if($nivel == 1){ ?>
+        |  <a href = "mudaracesso.php" > Mudar Acesso </a>
+        <?php } ?> <br>
+        <a href="logout.php">Sair</a>
     </center>
     
 </body>
